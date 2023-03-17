fd39ec312cf70d5cacc7c03f2f37eec3e26f9f5f
Sorongos <dosora9@gmail.com>
2023-03-17 17:01:45 +0900

    마이크 권한 허용

--- app/src/main/java/com/sorongos/recordapp/MainActivity.kt
+++ app/src/main/java/com/sorongos/recordapp/MainActivity.kt
@@ -1,15 +1,106 @@
 package com.sorongos.recordapp
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
+import androidx.appcompat.app.AlertDialog
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.sorongos.recordapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+        private const val REQUEST_RECORD_AUDIO_CODE = 200
+    }
+
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        
+
+        binding.recordButton.setOnClickListener {
+            when {
+                ContextCompat.checkSelfPermission(
+                    this,
+                    android.Manifest.permission.RECORD_AUDIO
+                ) == PackageManager.PERMISSION_GRANTED -> {
+                    // 실제 녹음 시작
+                }
+                //교육용 팝업
+                shouldShowRequestPermissionRationale(Manifest.permission.RECORD_AUDIO) -> {
+                    showPermissionInfoDialog()
+                }
+                //진짜 팝업
+                else -> {
+                    // You can directly ask for the permission.
+                    // The registered ActivityResultCallback gets the result of this request.
+                    requestRecordAudio()
+                }
+            }
+        }
+
+    }
+
+    private fun requestRecordAudio() {
+        ActivityCompat.requestPermissions(
+            this@MainActivity,
+            arrayOf(android.Manifest.permission.RECORD_AUDIO),
+            REQUEST_RECORD_AUDIO_CODE
+        )
+    }
+
+    private fun showPermissionInfoDialog() {
+        AlertDialog.Builder(this).apply {
+            setMessage("녹음을 위해 권한이 필요합니다.")
+            setNegativeButton("취소", null)
+            setPositiveButton("동의") { _, _ ->
+                requestRecordAudio()
+            }
+        }.show()
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        val audioRecordPermissionGranted = requestCode == REQUEST_RECORD_AUDIO_CODE //boolean
+                && grantResults.firstOrNull() == PackageManager.PERMISSION_GRANTED
+
+        if (audioRecordPermissionGranted) {
+            //todo
+
+        } else {
+            if (shouldShowRequestPermissionRationale(Manifest.permission.RECORD_AUDIO)) {
+                showPermissionInfoDialog()
+            } else { // 교육용 팝업을 봤는데도 허용 x
+                showPermissionSettingDialog()
+            }
+        }
+    }
+
+    private fun showPermissionSettingDialog() {
+        AlertDialog.Builder(this).apply {
+            setMessage("녹음 권한을 켜주셔야 앱을 정상적으로 사용할 수 있습니다. 설정에서 권한을 허용해 주세요.")
+            setNegativeButton("취소", null)
+            setPositiveButton("설정으로 이동") { _, _ ->
+                navigate2AppSetting()
+            }
+        }.show()
+    }
+
+    /**intent 함수*/
+    private fun navigate2AppSetting() {
+        val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS).apply {
+            data = Uri.fromParts("package", packageName, null)
+        }
+        startActivity(intent)
     }
 }
