83249db0d1394a19780d5260661a93c1ee96ccfb
Sorongos <dosora9@gmail.com>
2023-03-18 01:08:04 +0900

    녹음파형 움직이게

--- app/src/main/java/com/sorongos/recordapp/WaveformView.kt
+++ app/src/main/java/com/sorongos/recordapp/WaveformView.kt
@@ -14,21 +14,45 @@ class WaveformView @JvmOverloads constructor(
     defStyleAttr: Int = 0
 ) : View(context, attrs, defStyleAttr) {
 
-    val rectF = RectF(20f, 30f, 20f + 30f, 30f + 60f)
-    val redPaint = Paint().apply{
+    /**소리데이터, 계속 추가됨*/
+    private val ampList = mutableListOf<Float>()
+    /**그려질 애들의 데이터*/
+    private val rectList = mutableListOf<RectF>()
+
+    private val redPaint = Paint().apply{
         color = Color.RED
     }
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
 
-        canvas?.drawRect(rectF, redPaint)
+        for(rectF in rectList){
+            canvas?.drawRect(rectF, redPaint)
+        }
     }
 
     fun addAmplitude(maxAmplitude: Float) {
-        rectF.top = 0f
-        rectF.bottom = maxAmplitude
-        rectF.left = 0f
-        rectF.right = rectF.left + 20f
+
+        ampList.add(maxAmplitude/10)
+        rectList.clear()
+
+        /**하나의 rect의 width*/
+        val rectWidth = 10f
+        /**가로에 몇개의 rect?*/
+        val maxRect= (this.width/rectWidth).toInt()
+
+        //최신거 업데이트
+        val amps = ampList.takeLast(maxRect)
+
+        /**인덱스값과 함께 받아옴*/
+        for((i,amp) in amps.withIndex()){
+            val rectF = RectF()
+            rectF.top = 0f
+            rectF.bottom = amp
+            rectF.left = i * rectWidth // 오른쪽으로 x좌표가 늘어남
+            rectF.right = rectF.left + rectWidth
+
+            rectList.add(rectF)
+        }
 
         invalidate() //ondraw를 다시부름
     }
